289b89eb1df3c03c0d61e4fa3e8f1da51701e21e
ramasakti <ramasakti1337@gmail.com>
2026-04-29 05:42:17 +0700

    Stuktur user context yang menyesuaikan dari struktur cookie

--- app/type/User.ts
+++ app/type/User.ts
@@ -1,6 +1,17 @@
+export type Role = {
+  id_role: number;
+  role: string;
+}
+
+export type Kelas = {
+  id_kelas: number;
+  
+}
+
 export type User = {
   id: number;
+  username: string;
   name: string;
   email: string;
-  role?: string;
+  role: Role;
 }

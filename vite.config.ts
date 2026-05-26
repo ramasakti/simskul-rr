01ae7226e752e662aff341d8c526cec1d039e35b
ramasakti <ramasakti1337@gmail.com>
2026-05-27 06:25:07 +0700

    Import alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,15 @@
 import { reactRouter } from "@react-router/dev/vite";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
+import path from "path"
 
 export default defineConfig({
   plugins: [tailwindcss(), reactRouter()],
   resolve: {
     tsconfigPaths: true,
+    alias: {
+      "@": path.resolve(__dirname, "./app"),
+      "~": path.resolve(__dirname, "./app"),
+    },
   },
 });
